0f153a128e5de02ac2b3ec5cf6905dff1390ff73
yusakuNakano <senpukuzidouketu@gmail.com>
2021-08-31 15:02:04 +0900

    use non-null-aertion.

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -17,13 +17,18 @@ const Counter: React.FC<{}> = () => {
   useEffect(() => {
     renderTimes.current = renderTimes.current + 1;
   });
-
+  const ref = useRef<HTMLInputElement>(null!);
+  const focusInput = () => {
+    ref.current.focus();
+  };
   return (
     <div>
       <div>value: {value}</div>
       <button onClick={increment}>+1</button>
       <button onClick={decriment}>-1</button>
       <div>This component was re-rendered {renderTimes.current} times!</div>
+      <input ref={ref} type="text" />
+      <button onClick={focusInput}>Click Me!</button>
     </div>
   );
 };
